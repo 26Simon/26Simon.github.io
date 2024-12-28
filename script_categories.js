d90799b67c3bdb27a6bc860f98bb31552792732f
Simon Brenner <simonbrenner2460@gmail.com>
2024-12-28 21:59:37 +0100

    Updated "Wer weiß denn sowas?"

--- script_categories.js
+++ script_categories.js
@@ -1,3 +1,9 @@
+const audio = new Audio("soundtrack/DieWand.mp3");
+
+function playAudio() {
+    audio.play();
+}
+
 function solveNone(Id) {
     let element = document.getElementById(Id);
 
@@ -12,5 +18,18 @@ function solveTeamFarming(Id) {
 
     element.style.border='6px solid orange';
     element.style.boxShadow='none';
+    element.style.padding='16px';
     //element.style.boxShadow='6px 6px 5px -4px orange, -6px -6px 5px -4px orange, 6px -6px 5px -4px orange, -6px 6px 5px -4px orange';
+    element.firstElementChild.style.color='orange';
+    element.firstElementChild.innerHTML='Team Landwirtschaft';
+}
+
+function solveTeamEllwangen(Id) {
+    let element = document.getElementById(Id);
+
+    element.style.border='6px solid orange';
+    element.style.boxShadow='none';
+    //element.style.boxShadow='6px 6px 5px -4px orange, -6px -6px 5px -4px orange, 6px -6px 5px -4px orange, -6px 6px 5px -4px orange';
+    element.firstElementChild.style.color='orange';
+    element.firstElementChild.innerHTML='Team Ellwangen';
 }
---
+++ script_intro.js
@@ -0,0 +1,10 @@
+const audio = new Audio("./soundtrack/Intro.mp3");
+
+function start() {
+    audio.play();
+}
+
+function showCategories() {
+    let href = "WWDS.html";
+    window.location.href = href;
+}
